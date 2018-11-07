468e4f6ea29fcbf391983c9b9d52baced00bb711
xuyy121840 <xuyy163.com>
2018-11-07 08:35:35 +0800

    项目完成

---
+++ src/js/index.js
@@ -0,0 +1,18 @@
+require(['jquery'], function($) {
+    console.log($);
+})
+
+
+// function require() {
+//     //该方法可以导入模块
+//     //请写出内部逻辑
+
+//     return define;
+
+// };
+
+// function define() {
+//     //该方法可以定义模块
+//     //请写出内部逻辑
+//     var arr = [1, 2, 3, 6, 9];
+// }
